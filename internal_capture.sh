6c11c7df8a1e10f50d3b89bc830d7380ede9fe86
Sune Jakobsson <sune.jakobsson@comoyo.com>
2014-02-17 20:44:01 +0100

    corrected capture

--- internal_capture.sh
+++ internal_capture.sh
@@ -4,12 +4,14 @@
 if [ ! -d /home/pi/tmp ]
 then
 mkdir /home/pi/tmp
+sudo chgrp /home/pi/tmp/ pi
+sudo chown /home/pi/tmp/ pi
 sudo mount -t tmpfs /dev/ram15 /home/pi/tmp
 fi
  
 if [ -d /home/pi/tmp ] 
 then
-raspistill -v -o test.jpeg
-scp test.jpeg  sune@192.168.1.21:/var/www/garage/`date +%m`_`date +%b`_`date +%Y`/`date +%d`/cam0_`date +%Y`_`date +%b`_`date +%d`_`date +%H``date +%M`.jpeg
+raspistill -v -o /home/pi/tmp/test.jpeg
+scp /home/pi/tmp/test.jpeg  sune@192.168.1.21:/var/www/room/`date +%m`_`date +%b`_`date +%Y`/`date +%d`/cam0_`date +%Y`_`date +%b`_`date +%d`_`date +%H``date +%M`.jpeg
 echo "Cam 0" `date`
 fi
