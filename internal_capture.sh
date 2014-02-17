1ff56e03fa63d688d602f0bc8c349fdb9c618fd6
Sune Jakobsson <sune.jakobsson@comoyo.com>
2014-02-17 21:29:21 +0100

    corrected capture

--- internal_capture.sh
+++ internal_capture.sh
@@ -1,17 +1,26 @@
 #
 # capture from internal cam
 #
+# check if tmp directory exists.
+# if not create it and use ramdisk 15
+#
 if [ ! -d /home/pi/tmp ]
 then
 mkdir /home/pi/tmp
 sudo mount -t tmpfs /dev/ram15 /home/pi/tmp
-sudo chgrp /home/pi/tmp/ pi
-sudo chown /home/pi/tmp/ pi
+sudo chgrp pi /home/pi/tmp/
+sudo chown pi /home/pi/tmp/ 
 fi
- 
+#
+# check if tmp directory exists
+# if it does, capture one image
+# 
 if [ -d /home/pi/tmp ] 
 then
 raspistill -v -o /home/pi/tmp/test.jpeg
+#
+# copy the file to your server
+#
 scp /home/pi/tmp/test.jpeg  sune@192.168.1.21:/var/www/room/`date +%m`_`date +%b`_`date +%Y`/`date +%d`/cam0_`date +%Y`_`date +%b`_`date +%d`_`date +%H``date +%M`.jpeg
-echo "Cam 0" `date`
+echo "Internal Cam" `date`
 fi
