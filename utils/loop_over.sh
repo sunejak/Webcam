a0e604a3ab83bbc0ebe13bd8c0fa36f0a0c9bb8c
Sune Jakobsson <sune.jakobsson@comoyo.com>
2015-04-11 22:52:07 +0200

    Updated scripts for cam

--- utils/loop_over.sh
+++ utils/loop_over.sh
@@ -1,10 +1,26 @@
-source=cam*.jpeg; 
+#!/bin/bash
 
-echo $source;
+factor=$2
+if [[ ${factor} == "" ]] ; then
+    echo "Usage: loop_over.sh filepattern factor"
+    exit 1
+fi
+filepattern=$1
+if [[ ${filepattern} == "" ]] ; then
+    echo "Usage: loop_over.sh filepattern factor"
+    exit 1
+fi
 
-echo Process
+# array4=( $( ls cam0_2015_Apr_08_07*.jpeg) )
+array4=( $( ls ${filepattern}) )
+array4Len=(${#array4[@]})-1
 
-for ((i=0; i < ${#source[@]}; i++))
-do
-  echo ${source[$i]}
-done
+echo Lenght: $array4Len
+
+for ((i=0; i<$array4Len; i++))  
+do 
+	echo $i  ${array4[$i]}  ${array4[$i+1]}
+	java -cp ~/scripts/utils-1.0-SNAPSHOT.jar com.sune.app.XorImages ${array4[$i]}  ${array4[$i+1]} diff_${array4[$i]} ${factor}
+done 
+
+exit 0
