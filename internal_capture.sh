c5898ed0ad1be3d0042e7854e79f27f68e97943f
Sune Jakobsson <sune.jakobsson@comoyo.com>
2016-05-25 23:08:29 +0200

    Updated capture file to take delay parameter

--- internal_capture.sh
+++ internal_capture.sh
@@ -2,29 +2,36 @@
 #
 # capture from internal cam
 #
+delay=$1
+if [[ ${delay} == "" ]] ; then
+    echo "Usage:  internal_caapture.sh delaytime"
+    exit 1
+fi
+#
+sleep ${delay}s
+#
 # check if tmp directory exists.
 # if not create it and use ramdisk 15
 #
-if [ ! -d /home/pi/tmp ]
-then
-mkdir /home/pi/tmp
-sudo mount -t tmpfs /dev/ram15 /home/pi/tmp
-sudo chgrp pi /home/pi/tmp/
-sudo chown pi /home/pi/tmp/ 
+if [ ! -d /home/pi/tmp ] ; then
+    mkdir /home/pi/tmp
+    sudo mount -t tmpfs /dev/ram15 /home/pi/tmp
+    sudo chgrp pi /home/pi/tmp/
+    sudo chown pi /home/pi/tmp/
 fi
 #
 # check if tmp directory exists
 # if it does, capture one image
 # 
-if [ -d /home/pi/tmp ] 
-then
+if [ -d /home/pi/tmp ] ; then
 # for debug details
 # raspistill -v -o /home/pi/tmp/test.jpeg
 # for silent mode
-raspistill -n -o /home/pi/tmp/test.jpeg
+    raspistill -n -o /home/pi/tmp/test_${delay}.jpeg
 #
 # copy the file to your server
 #
-scp /home/pi/tmp/test.jpeg  sune@192.168.1.21:/var/www/room/`date +%m`_`date +%b`_`date +%Y`/`date +%d`/cam0_`date +%Y`_`date +%b`_`date +%d`_`date +%H``date +%M`.jpeg
-echo "Internal Cam" `date`
+    scp /home/pi/tmp/test_${delay}.jpeg  ttjsun@192.168.1.24:/var/www/html/garage.jpeg
+    scp /home/pi/tmp/test_${delay}.jpeg  ttjsun@192.168.1.24:/var/www/html/garage/`date +%m`_`date +%b`_`date +%Y`/`date +%d`/cam0_`date +%Y`_`date +%b`_`date +%d`_`date +%H``date +%M`_${delay}.jpeg
+    echo "Internal Cam" `date` ${delay}
 fi
